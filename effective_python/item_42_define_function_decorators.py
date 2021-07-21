aeec34942fc969ac454517156bc77f8fa1ac5467
yltsai0609 <yltsai0609@gmail.com>
2021-07-22 00:38:38 +0800

    [Feature] add attr kw_only and slot

--- effective_python/item_42_define_function_decorators.py
+++ effective_python/item_42_define_function_decorators.py
@@ -1,6 +1,6 @@
-'''
+"""
 Item 42 : Define function decorators with functools.wraps
-'''
+"""
 
 from functools import wraps
 
@@ -16,14 +16,14 @@ from functools import wraps
 # You want to print the arguments and return value of a function call
 # This is especially helpful when debugging s stack of function calls from a recursive function
 
+
 def trace(func):
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
-        print(
-            '%s(%r, %r -> %r' % (func.__name__, args, kwargs, result)
-        )
+        print("%s(%r, %r -> %r" % (func.__name__, args, kwargs, result))
         return result
     return wrapper
+
 
 # then I can apply this to a function using @ symbol
 
@@ -37,14 +37,16 @@ def fibonacci(n):
         return n
     return fibonacci(n - 2) + fibonacci(n - 1)
 
-# The @ symbol is equivalent to calling the decoratro on  the function it wraps
+
+# The @ symbol is equivalent to calling the decorator on the function it wraps
 # and assigning the return value to the original name in the same scope
 # just like
 
 # fibonacci = trace(fibonacci)
 
 
-fibonacci(3)
+r = fibonacci(3)
+print(r)
 
 # fibonacci((1,), {}) -> 1
 # fibonacci((0,), {}) -> 0
@@ -56,68 +58,68 @@ fibonacci(3)
 # The value retuened by the decorator-- the function that called above--doesn't thikn it's named
 # fibonacci
 
-print(fibonacci)
+# print(fibonacci)
 
-# <function trace.<locals>.wrapper at 0x7fac26042a60>
+# # <function trace.<locals>.wrapper at 0x7fac26042a60>
 
-# This cause of this isn't hard to see.
-# The trace function returns the wrapper it defines
-# The wrapper function is what's assigned to the fibonacci name in
-# the containing module because it undermines tools that do introspection
-
-
-def raw_fibonacci(n):
-    """
-    Return the n-th FIbonacci number
-    """
-    if n in (0, 1):
-        return n
-    return fibonacci(n - 2) + fibonacci(n - 1)
-
-# for example, help() on the fibonacci doesn't work at all
+# # This cause of this isn't hard to see.
+# # The trace function returns the wrapper it defines
+# # The wrapper function is what's assigned to the fibonacci name in
+# # the containing module because it undermines tools that do introspection
 
 
-# help(raw_fibonacci)
+# def raw_fibonacci(n):
+#     """
+#     Return the n-th FIbonacci number
+#     """
+#     if n in (0, 1):
+#         return n
+#     return fibonacci(n - 2) + fibonacci(n - 1)
+
+
+# # for example, help() on the fibonacci doesn't work at all
+
+
+# # help(raw_fibonacci)
+# # help(fibonacci)
+
+# # The solution is to use the wraps helper function from the functools built-in module
+# # This is a decorator that helps you write decorator.
+# # Applying it to the wrapper function will copy all of the important-meta-data about the innfer function
+# # to the outer function
+
+
+# def trace(func):
+#     @wraps(func)
+#     def wrapper(*args, **kwargs):
+#         result = func(*args, **kwargs)
+#         print("%s(%r, %r) -> %r", (func.__name__, args, kwargs, result))
+#         return result
+
+#     return wrapper
+
+
+# @trace
+# def fibonacci(n):
+#     """
+#     Return the n-th Fibonacci number
+#     """
+#     if n in (0, 1):
+#         return n
+#     return fibonacci(n - 2) + fibonacci(n - 1)
+
+
 # help(fibonacci)
+# # Now the "help" is working
 
-# The solution is to use the wraps helper function from the functools built-in module
-# This is a decorator that helps you write decorator.
-# Applying it to the wrapper function will copy all of the important-meta-data about the innfer function
-# to the outer function
+# # Calling help is just one example of how decoratros can subtly cause problems!
+# # python functions have many other standard attributes
+# # e.g. __name__, __module__, that must be preserved to maintian the interface of functions
+# # in the language
+# # Using wraps ensures that you'll always get the correct behavior
 
-def trace(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        result = func(*args, **kwargs)
-        print(
-            '%s(%r, %r) -> %r',
-            (func.__name__, args, kwargs, result)
-        )
-        return result
-    return wrapper
-
-
-@trace
-def fibonacci(n):
-    """
-    Return the n-th Fibonacci number
-    """
-    if n in (0, 1):
-        return n
-    return fibonacci(n - 2) + fibonacci(n - 1)
-
-
-help(fibonacci)
-# Now the "help" is working
-
-# Calling help is just one example of how decoratros can subtly cause problems!
-# python functions have many other standard attributes
-# e.g. __name__, __module__, that must be preserved to maintian the interface of functions
-# in the language
-# Using wraps ensures that you'll always get the correct behavior
-
-# Things to remember
-# 1. Decorators are Python syntax for allowing one function to modify another function st runtime
-# 2. Using decorators can cause strange behavior in tools that do introspection, such as debuggers
-# 3. Use the wraps decorator from the functools built-in module
-# when you define your own decorators to avoid any issues.
+# # Things to remember
+# # 1. Decorators are Python syntax for allowing one function to modify another function st runtime
+# # 2. Using decorators can cause strange behavior in tools that do introspection, such as debuggers
+# # 3. Use the wraps decorator from the functools built-in module
+# # when you define your own decorators to avoid any issues.
--- pixnet/item_1_attrs_python_oop.py
+++ pixnet/item_1_attrs_python_oop.py
@@ -6,7 +6,15 @@ introduction 2 : https://zhuanlan.zhihu.com/p/66267212
 
 github : https://github.com/python-attrs/attrs
 
+attr.s (s for self)
+attr.ib (ib for attribute ? )
+
+kw_only - set variable of class using keyword only
+slots=True - use __slot__ for attributes(it's will be faster when you are creating short-lifetime object)
+
 """
+import os
+
 import attr
 
 
@@ -30,7 +38,8 @@ class Color:
         pass
 
 
-# automatic generate __init__, __repr__, __eq__, __ne__, __lt__, __le__, __gt__, __ge__, __hash__
+# automatic generate __init__, __repr__, __eq__,
+# __ne__, __lt__, __le__, __gt__, __ge__, __hash__
 
 # __ge__ greater equal
 # __gt__ geater than
@@ -43,6 +52,16 @@ class GreaterColor(object):
     b = attr.ib(default=255, type=int)
 
 
+@attr.s(frozen=True, auto_attribs=True)
+class DataUri:
+    basename: str = attr.ib(converter=str)
+    prefix: str = attr.ib(converter=str)
+
+    @property
+    def fullpath(self):
+        return os.path.join(self.prefix, self.basename)
+
+
 c1 = Color(255, 255, 255)
 print(c1)
 c2 = GreaterColor()
@@ -52,6 +71,10 @@ print(c2 == GreaterColor(255, 255, 255))  # __eq__
 print(c2 > GreaterColor(255, 254, 0))  # __le__
 
 print(c2.__class__.__name__, dir(c2))
-print(attr.fields(c2))
 print()
 print(c1.__class__.__name__, dir(c1))
+
+
+d1 = DataUri(basename="abc.py", prefix="root")
+print(d1, d1.fullpath)
+# d1.basename = 4  # raise frozen error
