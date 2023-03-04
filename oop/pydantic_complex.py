65353c9d16944bfb16f241a27f8e949bad3cbd2c
yltsai0609 <yltsai0609@gmail.com>
2023-03-04 22:06:43 +0800

    feat: add error_handling for api

--- oop/pydantic_complex.py
+++ oop/pydantic_complex.py
@@ -9,28 +9,33 @@ from pydantic import BaseModel
 
 
 class Image(BaseModel):
-    url: str
-    width: int
-    height: int
+    url: str = None
+    width: int = None
+    height: int = None
 
 
 class Article(BaseModel):
-    article_id: int
-    images: List[Image]
+    article_id: int = None
+    images: List[Image] = None
+    error_msg : str = None
 
     @classmethod
-    def from_api(cls, endpoint: str = None):
+    def from_api(cls, endpoint: str = None, api_error : bool = False):
         time.sleep(0.5)  # to simulate request
-
-        fetched = {
+        if not api_error:
+            fetched = {
             "article_id": "50",
             "images": [
                 {"url": "i1", "width": "1", "height": "2"},
                 {"url": "i2", "width": "3", "height": "4"},
-            ],
-        }
+                ],
+            }
 
-        return cls(**fetched)
+            return cls(**fetched)
+        else:
+            err = {"err_msg" :'您沒有權限'}
+            return cls(**err)
+
 
     def infer_spark_schema():
         pass
@@ -38,3 +43,4 @@ class Article(BaseModel):
 
 print(Article.from_api())
 print(dict(Article.from_api()), Article.from_api().dict(), Article.from_api().json(), sep="\n\n")
+print(Article.from_api(api_error=True).dict())
